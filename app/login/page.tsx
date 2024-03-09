478544bd726b7173c5dc2c55adc801cfe860d5bd
rick-sys <ricksanchezy2x@gmail.com>
2024-03-10 00:47:16 +0100

    erre

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -28,66 +28,7 @@ export default async function Profile({ params }) {
 
 export const dynamic = "force-dynamic";
 
-export default async function Login() {
-  return (
-    <div className="flex-1 flex flex-col w-full py-6 sm:max-w-md justify-center gap-2">
 
-
-<Tabs defaultValue="account" className="w-[400px] transition-all">
-  <TabsList className="w-full bg-transparent">
-    <TabsTrigger value="account" className="text-start shadow-lg">Male Version</TabsTrigger>
-    <Link href="" className="text-sm text-gray-500 ml-4">
-      <p>Go to Male Version</p>
-      </Link>
-  </TabsList>
-  <TabsContent value="account" >
-      <form
-        className="flex-1 flex flex-col w-full justify-center gap-2 rounded-full shadow-xl"
-        action="/auth/sign-in"
-        method="post"
-      >
-        <Card className="border-0 shadow-2xl rounded-2xl">
-          <CardHeader>
-            <CardTitle>Log In / Sign Up</CardTitle>
-            <CardDescription>
-              Log into your account or sign up for a new one to get started.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="grid gap-4">
-            <Label className="text-md" htmlFor="email">
-              Email
-            </Label>
-            <input
-              className="rounded-full px-4 py-2 bg-inherit border"
-              name="email"
-              placeholder="you@example.com"
-              required
-            />
-            <Button className="shadow-lg h-10 animate-pulse hover:animate-none">Continue</Button>
-            <Messages />
-          </CardContent>
-          <CardFooter>
-            <p className="text-xs text-gray-400">
-              By signing up, you agree to our{" "}
-              <a href="#" className="underline">
-                Terms of Service
-              </a>{" "}
-              and{" "}
-              <a href="#" className="underline">
-                Privacy Policy
-              </a>
-              .
-            </p>
-          </CardFooter>
-        </Card>
-      </form>
-  </TabsContent>
-  <TabsContent value="password">Change your password here.</TabsContent>
-</Tabs>
-
-    </div>
-  );
-}
 
 function LinkIcon() {
   return (
