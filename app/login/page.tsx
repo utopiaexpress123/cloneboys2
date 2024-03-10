af69e43c1a3c6f52bd0998af6abcebdc518fee4c
rick-sys <ricksanchezy2x@gmail.com>
2024-03-10 01:08:07 +0100

    rtt

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -9,26 +9,71 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Badge } from "@/components/ui/badge";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Link from "next/link";
-import { redirect } from 'next/navigation';
-async function fetchTeam(id) {
-  const res = await fetch('https://...')
-  if (!res.ok) return undefined
-  return res.json()
-}
- 
-export default async function Profile({ params }) {
-  const team = await fetchTeam(params.id)
-  if (!team) {
-    redirect('/login')
-  }
-}
 
 export const dynamic = "force-dynamic";
 
+export default async function Login() {
+  return (
+    <div className="flex-1 flex flex-col w-full py-6 sm:max-w-md justify-center gap-2">
 
+
+<Tabs defaultValue="account" className="w-[400px] transition-all">
+  <TabsList className="w-full bg-transparent">
+    <TabsTrigger value="account" className="text-start shadow-lg">Male Version</TabsTrigger>
+    <Link href="" className="text-sm text-gray-500 ml-4">
+      <p>Go to Female Version</p>
+      </Link>
+  </TabsList>
+  <TabsContent value="account" >
+      <form
+        className="flex-1 flex flex-col w-full justify-center gap-2 rounded-full shadow-xl"
+        action="/auth/sign-in"
+        method="post"
+      >
+        <Card className="border-0 shadow-2xl rounded-2xl">
+          <CardHeader>
+            <CardTitle>Log In / Sign Up</CardTitle>
+            <CardDescription>
+              Log into your account or sign up for a new one to get started.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <Label className="text-md" htmlFor="email">
+              Email
+            </Label>
+            <input
+              className="rounded-full px-4 py-2 bg-inherit border"
+              name="email"
+              placeholder="you@example.com"
+              required
+            />
+            <Button className="shadow-lg h-10 animate-pulse hover:animate-none">Continue</Button>
+            <Messages />
+          </CardContent>
+          <CardFooter>
+            <p className="text-xs text-gray-400">
+              By signing up, you agree to our{" "}
+              <a href="#" className="underline">
+                Terms of Service
+              </a>{" "}
+              and{" "}
+              <a href="#" className="underline">
+                Privacy Policy
+              </a>
+              .
+            </p>
+          </CardFooter>
+        </Card>
+      </form>
+  </TabsContent>
+  <TabsContent value="password">Change your password here.</TabsContent>
+</Tabs>
+
+    </div>
+  );
+}
 
 function LinkIcon() {
   return (
--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,25 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import Link from "next/link";
 import { redirect } from "next/navigation";
-import hero from "/public/hero.png";
-//import AutoplayVideo from "@/components/ui/autoplayvideo";
+import hero from "../public/hero.png";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+
 //import { Button } from "@/components/ui/button";
 //import ExplainerSection from "@/components/ExplainerSection";
 //import PricingSection from "@/components/PricingSection";
-//import UtopiaLogo from "@/components/UtopiaLogo";
 import { MainHero } from "@/components/component/main-hero";
 import { HowItWoks } from "@/components/component/how-it-woks";
 import { FAQ } from "@/components/component/faq";
 
 export const dynamic = "force-dynamic";
+
+
 
 export default async function Index() {
   const supabase = createServerComponentClient({ cookies });
@@ -26,7 +34,6 @@ export default async function Index() {
   }
 
   return (
-
     <div className="flex flex-col items-center pt-0">
       <div className="flex flex-col lg:flex-row items-center gap-8 p-8 max-w-6xl w-full">
         <div className="flex flex-col space-y-4 lg:w-1/2 w-full">
@@ -45,8 +52,8 @@ export default async function Index() {
           />
         </div>
       </div>
-      <div className="flex flex-col lg:flex-row items-center gap-8 p-8 w-full">
-      <FAQ/>
+      <div className="flex-col flex gap-8 p-8 max-w-6xl w-full">
+        <FAQ/>
       </div>
     </div>
   );
